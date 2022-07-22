dfd7ad6fa7291a98a842e8e773854ded54a5f4ed
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-07-22 07:36:58 -0400

    feat: hero h1 typewriter component and styles

---
+++ src/components/Name.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import Typewriter from "typewriter-effect";
+import styles from "../styles/name.module.scss";
+
+const Name: FC = () => {
+  return (
+    <h1 className={styles.name}>
+      <Typewriter
+        onInit={(typewriter): void => {
+          typewriter
+            .typeString("Christian Fugraino")
+            .pauseFor(1000)
+            .deleteChars(5)
+            .typeString("arino")
+            .start();
+        }}
+      />
+    </h1>
+  );
+};
+
+export default Name;
